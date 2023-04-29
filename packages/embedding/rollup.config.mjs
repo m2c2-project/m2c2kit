22a7281f82a8d0a3c0b014dd15dd9ab147372028
Scott Yabiku <syabiku@gmail.com>
2023-04-29 15:33:46 -0400

    add @m2c2kit/embedding package

---
+++ packages/embedding/rollup.config.mjs
@@ -0,0 +1,65 @@
+import esbuild from "rollup-plugin-esbuild";
+import { minify } from "rollup-plugin-esbuild";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+import replace from "@rollup/plugin-replace";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url), "utf8")
+);
+const insertVersionString = replace({
+  __PACKAGE_JSON_VERSION__: pkg.version,
+  preventAssignment: true,
+});
+
+export default [
+  {
+    input: ["./src/index.ts"],
+    external: ["@m2c2kit/core"],
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      insertVersionString,
+      esbuild(),
+      copy({
+        targets: [
+          {
+            src: "build/index.d.ts",
+            dest: "dist",
+          },
+        ],
+      }),
+    ],
+  },
+  {
+    input: ["./src/index.ts"],
+    output: [
+      {
+        file: "./build-nobundler/m2c2kit.embedding.esm.min.js",
+        format: "es",
+        sourcemap: false,
+        plugins: [minify()],
+      },
+    ],
+    plugins: [
+      insertVersionString,
+      nodeResolve(),
+      esbuild(),
+      copy({
+        targets: [
+          {
+            src: "build/index.d.ts",
+            dest: "build-nobundler",
+            rename: () => "m2c2kit.embedding.esm.min.d.ts",
+          },
+        ],
+      }),
+    ],
+  },
+];
