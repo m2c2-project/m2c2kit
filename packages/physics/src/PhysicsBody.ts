6bc492781b67bc809cd63bbf099f406513ff8fbc
Scott Yabiku <syabiku@gmail.com>
2023-06-28 13:22:41 -0400

    Add edgeloop to physics

--- packages/physics/src/PhysicsBody.ts
+++ packages/physics/src/PhysicsBody.ts
@@ -12,6 +12,22 @@ export interface PhysicsBodyOptions {
    */
   rect?: Size;
   /**
+   * A region that physics bodies cannot penetrate.
+   */
+  edgeLoop?: Size & {
+    /**
+     * The thickness of the edge loop. Defaults to 10.
+     *
+     * @remarks If body A is moving rapidly and/or is small, and body B is
+     * also small, collisions between the two bodies may not be detected.
+     * This can manifest as body A "passing through" body B. This is called
+     * "tunneling". If body B is an edge loop, one way to reduce tunneling
+     * is to increase the thickness of the edge loop.
+     * See https://github.com/liabru/matter-js/issues/5
+     */
+    thickness?: number;
+  };
+  /**
    * Whether or not the physics body moves in response to forces. Defaults to true.
    *
    * @remarks Once set, this property cannot be changed. This is negated and mapped to the Matter.js `static` property.
@@ -46,6 +62,7 @@ export class PhysicsBody {
   private _isDynamic = true;
   private _resting = false;
   private _restitution = 0;
+  private readonly EDGE_LOOP_DEFAULT_THICKNESS = 10;
 
   get body() {
     if (!this._body) {
@@ -70,6 +87,11 @@ export class PhysicsBody {
   }
 
   set isDynamic(isDynamic: boolean) {
+    if (this.options.edgeLoop) {
+      throw new Error(
+        "PhysicsBody.isDynamic cannot be set after the PhysicsBody edgeLoop has been created."
+      );
+    }
     this._isDynamic = isDynamic;
     Matter.Body.setStatic(this.body, !this.options.isDynamic);
   }
@@ -141,6 +163,116 @@ export class PhysicsBody {
         });
         this.entity.addChild(rectOutline);
       }
+    } else if (this.options.edgeLoop) {
+      const thickness =
+        this.options.edgeLoop.thickness ?? this.EDGE_LOOP_DEFAULT_THICKNESS;
+
+      const partA = Matter.Bodies.rectangle(
+        this.entity.position.x,
+        this.entity.position.y -
+          this.options.edgeLoop.height / 2 -
+          thickness / 2,
+        this.options.edgeLoop.width + thickness * 2,
+        thickness
+      );
+      const partB = Matter.Bodies.rectangle(
+        this.entity.position.x,
+        this.entity.position.y +
+          this.options.edgeLoop.height / 2 +
+          thickness / 2,
+        this.options.edgeLoop.width + thickness * 2,
+        thickness
+      );
+      const partC = Matter.Bodies.rectangle(
+        this.entity.position.x -
+          this.options.edgeLoop.width / 2 -
+          thickness / 2,
+        this.entity.position.y,
+        thickness,
+        this.options.edgeLoop.height
+      );
+      const partD = Matter.Bodies.rectangle(
+        this.entity.position.x +
+          this.options.edgeLoop.width / 2 +
+          thickness / 2,
+        this.entity.position.y,
+        thickness,
+        this.options.edgeLoop.height
+      );
+      this.body = Matter.Body.create({
+        parts: [partA, partB, partC, partD],
+        isStatic: true,
+      });
+
+      if (Physics.options.showsPhysics) {
+        const rectOutlineA = new Shape({
+          position: {
+            x: 0,
+            y: -this.options.edgeLoop.height / 2 - thickness / 2,
+          },
+          rect: {
+            width: this.options.edgeLoop.width + thickness * 2,
+            height: thickness,
+          },
+          fillColor: WebColors.Transparent,
+          strokeColor:
+            Physics.options.showsPhysicsStrokeColor ?? WebColors.Green,
+          lineWidth: Physics.options.showsPhysicsLineWidth ?? 1,
+          zPosition: Number.MAX_SAFE_INTEGER,
+        });
+        this.entity.addChild(rectOutlineA);
+
+        const rectOutlineB = new Shape({
+          position: {
+            x: 0,
+            y: this.options.edgeLoop.height / 2 + thickness / 2,
+          },
+          rect: {
+            width: this.options.edgeLoop.width + thickness * 2,
+            height: thickness,
+          },
+          fillColor: WebColors.Transparent,
+          strokeColor:
+            Physics.options.showsPhysicsStrokeColor ?? WebColors.Green,
+          lineWidth: Physics.options.showsPhysicsLineWidth ?? 1,
+          zPosition: Number.MAX_SAFE_INTEGER,
+        });
+        this.entity.addChild(rectOutlineB);
+
+        const rectOutlineC = new Shape({
+          position: {
+            x: -this.options.edgeLoop.width / 2 - thickness / 2,
+            y: 0,
+          },
+          rect: {
+            width: thickness,
+            height: this.options.edgeLoop.height,
+          },
+          fillColor: WebColors.Transparent,
+          strokeColor:
+            Physics.options.showsPhysicsStrokeColor ?? WebColors.Green,
+          lineWidth: Physics.options.showsPhysicsLineWidth ?? 1,
+          zPosition: Number.MAX_SAFE_INTEGER,
+        });
+        this.entity.addChild(rectOutlineC);
+
+        const rectOutlineD = new Shape({
+          position: {
+            x: this.options.edgeLoop.width / 2 + thickness / 2,
+            y: 0,
+          },
+          rect: {
+            width: thickness,
+            height: this.options.edgeLoop.height,
+          },
+          fillColor: WebColors.Transparent,
+          strokeColor:
+            Physics.options.showsPhysicsStrokeColor ?? WebColors.Green,
+          lineWidth: Physics.options.showsPhysicsLineWidth ?? 1,
+          zPosition: Number.MAX_SAFE_INTEGER,
+        });
+        this.entity.addChild(rectOutlineD);
+      }
     } else {
       throw new Error(
         "PhysicsBodyOptions are invalid; must specify either circleOfRadius or rect"
@@ -149,6 +281,14 @@ export class PhysicsBody {
 
     if (this.options.isDynamic !== undefined) {
       this.isDynamic = this.options.isDynamic;
+    }
+    if (this.options.edgeLoop) {
+      if (this.options.isDynamic) {
+        throw new Error(
+          "PhysicsBodyOptions are invalid; edgeLoop bodies must be static"
+        );
+      }
+      this._isDynamic = false;
     }
     if (this.options.resting !== undefined) {
       this.resting = this.options.resting;
