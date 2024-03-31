4dddc7ed3e58e0b4fc84974ef29bfd6ea61cf98a
Scott Yabiku <syabiku@gmail.com>
2024-03-31 16:32:36 -0400

    Change eslint config
    Was plugin:@typescript-eslint/recommended, now
    plugin:@typescript-eslint/strict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
