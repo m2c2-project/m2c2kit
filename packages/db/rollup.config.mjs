be0485428bfba3673a52b5c4572c4c7a94d320be
Scott Yabiku <syabiku@gmail.com>
2024-04-23 15:27:21 -0400

    Update dependencies
    New version of dexie requires the @rollup/plugin-commonjs plugin in the
    rollup.config.mjs file.

--- packages/db/rollup.config.mjs
+++ packages/db/rollup.config.mjs
@@ -3,6 +3,7 @@ import esbuild from "rollup-plugin-esbuild";
 import { minify } from "rollup-plugin-esbuild";
 import copy from "rollup-plugin-copy";
 import dts from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
 
 // I could not get @rollup/plugin-typescript to work with the
 // emitDeclarationOnly option. Thus, as part of the build script in
@@ -16,24 +17,12 @@ export default [
     output: [
       { file: "./build/index.js", format: "es", sourcemap: true },
       {
-        file: "./build-nobundler/m2c2kit.db.esm.js",
+        file: "./build/index.min.js",
         format: "es",
-        paths: {
-          "@m2c2kit/core": "./m2c2kit.core.esm.js",
-        },
-        sourcemap: false,
-      },
-      {
-        file: "./build-nobundler/m2c2kit.db.esm.min.js",
-        format: "es",
-        paths: {
-          "@m2c2kit/core": "./m2c2kit.core.esm.min.js",
-        },
-        sourcemap: false,
         plugins: [minify()],
       },
     ],
-    plugins: [nodeResolve(), esbuild()],
+    plugins: [nodeResolve(), commonjs(), esbuild()],
   },
   {
     // bundle all declaration files and place the declaration
@@ -52,14 +41,8 @@ export default [
             dest: "dist",
           },
           {
-            src: "dist/index.d.ts",
-            dest: "build-nobundler/",
-            rename: () => "m2c2kit.db.esm.d.ts",
-          },
-          {
-            src: "dist/index.d.ts",
-            dest: "build-nobundler/",
-            rename: () => "m2c2kit.db.esm.min.d.ts",
+            src: "build/index.min.js",
+            dest: "dist",
           },
         ],
       }),
@@ -70,6 +53,7 @@ export default [
     output: [{ file: "./build/data.js", format: "es" }],
     plugins: [
       nodeResolve(),
+      commonjs(),
       esbuild(),
       copy({
         targets: [
@@ -83,75 +67,3 @@ export default [
     ],
   },
 ];
-
-// export default [
-//   {
-//     input: ["./src/index.ts"],
-//     // the output is build because we need a later step to
-//     // combine all declaration files
-//     output: [{ dir: "./build", format: "es", sourcemap: true }],
-//     plugins: [
-//       nodeResolve(),
-//       del({
-//         targets: ["dist/*", "build/*", "build-nobundler/*"],
-//       }),
-//       typescript({
-//         // I was getting errors when defining include and exclude
-//         // only in tsconfig.json, thus defining them here.
-//         // note, however, because I specified rootDir below,
-//         // the include and exclude now are relative to src
-//         include: ["./**/*.[tj]s"],
-//         exclude: ["**/__tests__", "**/*.test.ts"],
-//         rootDir: "src",
-//         outputToFilesystem: true,
-//       }),
-//     ],
-//   },
-
-//   {
-//     // bundle all declaration files and place the declaration
-//     // bundle in dist
-//     input: "./build/index.d.ts",
-//     output: [{ file: "dist/index.d.ts", format: "es" }],
-//     plugins: [
-//       dts(),
-//       copy({
-//         targets: [
-//           {
-//             // copy the bundled esm module and sourcemap to dist
-//             src: "build/index.*",
-//             dest: "dist",
-//           },
-//         ],
-//       }),
-//     ],
-//   },
-
-//   {
-//     input: ["./src/data.ts"],
-//     output: [{ file: "./build/data.js", format: "es" }],
-//     plugins: [
-//       nodeResolve(),
-//       typescript({
-//         // I was getting errors when defining include and exclude
-//         // only in tsconfig.json, thus defining them here.
-//         // note, however, because I specified rootDir below,
-//         // the include and exclude now are relative to src
-//         include: ["./**/*.[tj]s"],
-//         exclude: ["**/__tests__", "**/*.test.ts"],
-//         rootDir: "src",
-//         outputToFilesystem: true,
-//         sourceMap: false,
-//       }),
-//       copy({
-//         targets: [
-//           {
-//             src: "build/data.js",
-//             dest: "dist",
-//           },
-//         ],
-//         hook: "writeBundle",
-//       }),
-//     ],
-//   },
-// ];
